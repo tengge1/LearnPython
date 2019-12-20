2138e5ec5293cfbeefffa40ecf23fe8f571e2df7
liteng <liteng@w-ew.com>
2019-12-20 12:24:34 +0800

    58.conv_lstm

--- keras/58.conv_lstm.py
+++ keras/58.conv_lstm.py
@@ -1,7 +1,10 @@
 # Convolutional LSTM
 
+# https://keras.io/examples/conv_lstm/
+
 # This script demonstrates the use of a convolutional LSTM network.
-# This network is used to predict the next frame of an artificially generated movie which contains moving squares.
+# This network is used to predict the next frame of an artificially
+# generated movie which contains moving squares.
 
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
