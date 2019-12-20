cac4cc6c3b320e3e273e75aacc9de14d283df7bf
liteng <liteng@w-ew.com>
2019-12-20 12:14:18 +0800

    54.babi_memnn

--- keras/54.babi_memnn.py
+++ keras/54.babi_memnn.py
@@ -1,3 +1,20 @@
+# Baby MemNN
+
+# https://keras.io/examples/babi_memnn/
+
+# Trains a memory network on the bAbI dataset.
+# References:
+
+# Jason Weston, Antoine Bordes, Sumit Chopra, Tomas Mikolov, Alexander M. Rush,
+# "Towards AI-Complete Question Answering: A Set of Prerequisite Toy Tasks"
+# url: http://arxiv.org/abs/1502.05698
+
+# Sainbayar Sukhbaatar, Arthur Szlam, Jason Weston, Rob Fergus, "End-To-End Memory Networks"
+# url: http://arxiv.org/abs/1503.08895
+
+# Reaches 98.6% accuracy on task 'single_supporting_fact_10k' after 120 epochs.
+# Time per epoch: 3s on CPU (core i7).
+
 from tensorflow import keras
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Embedding
