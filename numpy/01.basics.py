40dd9db655d14411dcb9ab212c2045d9878ae374
liteng <liteng@w-ew.com>
2020-01-08 12:14:30 +0800

    01.basics

---
+++ numpy/01.basics.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([
+    [1, 0, 0],
+    [0, 1, 2]
+])
+
+print(arr.ndim)
+print(arr.shape)
+print(arr.size)
+print(arr.dtype)
+print(arr.itemsize)
+print(arr.data)
