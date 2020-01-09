9c41fd4fdd514a3a732c036d49cf06811e6dd8f7
liteng <liteng@w-ew.com>
2020-01-09 12:23:38 +0800

    01.hello

---
+++ python/liaoxuefeng/01.hello.py
@@ -0,0 +1,10 @@
+print('hello, world')
+
+print(100 + 200 + 300)
+
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+
+print('100 + 200 =', 100 + 200)
+
+name = input('Your name:')
+print('Hello,', name, '!')
