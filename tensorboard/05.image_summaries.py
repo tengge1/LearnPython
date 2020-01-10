84864a86d2d4e242cefd7780f2206064b8a2828e
liteng <liteng@w-ew.com>
2020-01-11 07:52:07 +0800

    05.image_summaries

--- tensorboard/05.image_summaries.py
+++ tensorboard/05.image_summaries.py
@@ -1,1 +1,37 @@
 # https://tensorflow.google.cn/tensorboard/image_summaries
+
+from datetime import datetime
+import io
+import itertools
+
+import tensorflow as tf
+from tensorflow import keras
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.metrics
+
+# Download the data. The data is already divided into train and test.
+# The labels are integers representing classes.
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = \
+    fashion_mnist.load_data()
+
+# Names of the integer classes, i.e., 0 -> T-short/top, 1 -> Trouser, etc.
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print("Shape: ", train_images[0].shape)
+print("Label: ", train_labels[0], "->", class_names[train_labels[0]])
+
+# Reshape the image for the Summary API.
+img = np.reshape(train_images[0], (-1, 28, 28, 1))
+
+# Sets up a timestamped log directory.
+logdir = "logs\\train_data\\" + datetime.now().strftime("%Y%m%d-%H%M%S")
+# Creates a file writer for the log directory.
+file_writer = tf.summary.create_file_writer(logdir)
+
+# Using the file writer, log the reshaped image.
+with file_writer.as_default():
+  tf.summary.image("Training data", img, step=0)
