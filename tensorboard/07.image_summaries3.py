ed08de39ea070dfdc82bc636262ccb8fb03fbacd
liteng <liteng@w-ew.com>
2020-01-11 12:35:25 +0800

    07.image_summaries3

--- tensorboard/07.image_summaries3.py
+++ tensorboard/07.image_summaries3.py
@@ -21,17 +21,6 @@ fashion_mnist = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-print("Shape: ", train_images[0].shape)
-print("Label: ", train_labels[0], "->", class_names[train_labels[0]])
-
-# Reshape the image for the Summary API.
-img = np.reshape(train_images[0], (-1, 28, 28, 1))
-
-# Sets up a timestamped log directory.
-logdir = "logs/plots/" + datetime.now().strftime("%Y%m%d-%H%M%S")
-# Creates a file writer for the log directory.
-file_writer = tf.summary.create_file_writer(logdir)
-
 
 def plot_to_image(figure):
   """Converts the matplotlib plot specified by 'figure' to a PNG image and
@@ -50,23 +39,81 @@ def plot_to_image(figure):
   return image
 
 
-def image_grid():
-  """Return a 5x5 grid of the MNIST images as a matplotlib figure."""
-  # Create a figure to contain the plot.
-  figure = plt.figure(figsize=(10, 10))
-  for i in range(25):
-    # Start next subplot.
-    plt.subplot(5, 5, i + 1, title=class_names[train_labels[i]])
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(32, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
 
-  return figure
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
 
 
-# Prepare the plot
-figure = image_grid()
-# Convert to image and log
-with file_writer.as_default():
-  tf.summary.image("Training data", plot_to_image(figure), step=0)
+def plot_confusion_matrix(cm, class_names):
+  """
+  Returns a matplotlib figure containing the plotted confusion matrix.
+
+  Args:
+    cm (array, shape = [n, n]): a confusion matrix of integer classes
+    class_names (array, shape = [n]): String names of the integer classes
+  """
+  figure = plt.figure(figsize=(8, 8))
+  plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+  plt.title("Confusion matrix")
+  plt.colorbar()
+  tick_marks = np.arange(len(class_names))
+  plt.xticks(tick_marks, class_names, rotation=45)
+  plt.yticks(tick_marks, class_names)
+
+  # Normalize the confusion matrix.
+  cm = np.around(cm.astype('float') / cm.sum(axis=1)
+                 [:, np.newaxis], decimals=2)
+
+  # Use white text if squares are dark; otherwise black.
+  threshold = cm.max() / 2.
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    color = "white" if cm[i, j] > threshold else "black"
+    plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+
+  plt.tight_layout()
+  plt.ylabel('True label')
+  plt.xlabel('Predicted label')
+
+
+logdir = "logs\\image\\" + datetime.now().strftime("%Y%m%d-%H%M%S")
+# Define the basic TensorBoard callback.
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+file_writer_cm = tf.summary.create_file_writer(logdir + '/cm')
+
+
+def log_confusion_matrix(epoch, logs):
+  # Use the model to predict the values from the validation dataset.
+  test_pred_raw = model.predict(test_images)
+  test_pred = np.argmax(test_pred_raw, axis=1)
+
+  # Calculate the confusion matrix.
+  cm = sklearn.metrics.confusion_matrix(test_labels, test_pred)
+  # Log the confusion matrix as an image summary.
+  figure = plot_confusion_matrix(cm, class_names=class_names)
+  cm_image = plot_to_image(figure)
+
+  # Log the confusion matrix as an image summary.
+  with file_writer_cm.as_default():
+    tf.summary.image("Confusion Matrix", cm_image, step=epoch)
+
+
+# Define the per-epoch callback.
+cm_callback = keras.callbacks.LambdaCallback(on_epoch_end=log_confusion_matrix)
+
+# Train the classifier.
+model.fit(
+    train_images,
+    train_labels,
+    epochs=5,
+    verbose=0,  # Suppress chatty output
+    callbacks=[tensorboard_callback, cm_callback],
+    validation_data=(test_images, test_labels),
+)
