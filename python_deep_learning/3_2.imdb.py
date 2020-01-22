9fe2e005f04dbfc1d1ec7e9f701b2e9cd8ec1f90
liteng <930372551@qq.com>
2020-01-22 15:14:01 +0800

    过拟合。

--- python_deep_learning/3_2.imdb.py
+++ python_deep_learning/3_2.imdb.py
@@ -62,7 +62,7 @@ partial_y_train = y_train[10000:]
 history = model.fit(
     partial_x_train,
     partial_y_train,
-    epochs=4,
+    epochs=20,
     batch_size=512,
     validation_data=(x_val, y_val)
 )
