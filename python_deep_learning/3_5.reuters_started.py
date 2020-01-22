d71281f977951c1e8531149b8aeebd1102799e76
liteng <930372551@qq.com>
2020-01-22 16:29:00 +0800

    3_5.reuters_started

---
+++ python_deep_learning/3_5.reuters_started.py
@@ -0,0 +1,51 @@
+from tensorflow.keras.datasets import reuters
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(
+    num_words=10000
+)
+
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+    return results
+
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+one_hot_train_labels = to_categorical(train_labels)
+one_hot_test_labels = to_categorical(test_labels)
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+model.compile(
+    optimizer='rmsprop',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+
+y_val = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
+
+history = model.fit(
+    partial_x_train,
+    partial_y_train,
+    epochs=9,
+    batch_size=512,
+    validation_data=(x_val, y_val)
+)
+
+results = model.evaluate(x_test, one_hot_test_labels)
+print(results)
