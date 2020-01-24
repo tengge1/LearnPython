8f1dca5e2e9f9f7e169ca2b71585e28a012783ca
liteng <930372551@qq.com>
2020-01-24 09:10:24 +0800

    5_1.mnist_convnet

--- python_deep_learning/5_1.mnist_convnet.py
+++ python_deep_learning/5_1.mnist_convnet.py
@@ -8,6 +8,9 @@ from tensorflow.keras.utils import to_categorical
 train_images = train_images.reshape((60000, 28, 28, 1))
 train_images = train_images.astype('float32') / 255
 
+test_images = test_images.reshape((10000, 28, 28, 1))
+test_images = test_images.astype('float32') / 255
+
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
 
