6cf60ced941e8d394ec31fac44960bd39c4eafdb
liteng <930372551@qq.com>
2020-01-28 15:11:48 +0800

    5_11.show_heatmap

--- python_deep_learning/5_11.show_heatmap.py
+++ python_deep_learning/5_11.show_heatmap.py
@@ -1,7 +1,15 @@
+import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
+from tensorflow.keras import backend as K
 import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+# pip install opencv-python
+
+# tensorflow 2.0 禁用eager模式
+tf.compat.v1.disable_eager_execution()
 
 model = VGG16(weights='imagenet')
 
@@ -18,3 +26,46 @@ x = preprocess_input(x)
 preds = model.predict(x)
 
 print('Predicted:', decode_predictions(preds, top=3)[0])
+
+pred = np.argmax(preds[0])
+
+african_elephant_output = model.output[:, pred]  # 预测向量中的非洲象元素
+
+last_conv_layer = model.get_layer('block5_conv3')
+
+grads = K.gradients(african_elephant_output, last_conv_layer.output)[0]
+
+pooled_grads = K.mean(grads, axis=(0, 1, 2))
+
+iterate = K.function(
+    [model.input],
+    [pooled_grads, last_conv_layer.output[0]]
+)
+
+pooled_grads_value, conv_layer_output_value = iterate([x])
+
+for i in range(512):
+    conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+
+heatmap = np.mean(conv_layer_output_value, axis=-1)
+
+heatmap = np.maximum(heatmap, 0)
+
+heatmap /= np.max(heatmap)
+
+plt.matshow(heatmap)
+
+plt.show()
+
+# 叠加图片
+img = cv2.imread(img_path)
+
+heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+
+heatmap = np.uint8(255 * heatmap)
+
+heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+
+superimposed_img = heatmap * 0.4 + img
+
+cv2.imwrite('element_cam.jpg', superimposed_img)
