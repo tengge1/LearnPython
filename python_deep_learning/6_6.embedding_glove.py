4d9382cc3206d78a16aeccb1022ea98d89a9b3a1
liteng <930372551@qq.com>
2020-01-29 18:36:38 +0800

    6_6.embedding_glove

--- python_deep_learning/6_6.embedding_glove.py
+++ python_deep_learning/6_6.embedding_glove.py
@@ -4,6 +4,7 @@ from tensorflow.keras.layers import Embedding, Flatten, Dense
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
+import matplotlib.pyplot as plt
 
 imdb_dir = 'D:\\aclImdb'
 train_dir = os.path.join(imdb_dir, 'train')
@@ -14,8 +15,8 @@ texts = []
 for label_type in ['neg', 'pos']:
     dir_name = os.path.join(train_dir, label_type)
     for fname in os.listdir(dir_name):
-        if fname[-4:] == = '.txt':
-            f = open(os.path.join(dir_name, fname))
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname), encoding='utf-8')
             texts.append(f.read())
             f.close()
             if label_type == 'neg':
@@ -33,11 +34,13 @@ tokenizer.fit_on_texts(texts)
 sequences = tokenizer.texts_to_sequences(texts)
 
 word_index = tokenizer.word_index
+
 print('Found %s unique tokens.' % len(word_index))
 
 data = pad_sequences(sequences, maxlen=maxlen)
 
 labels = np.asarray(labels)
+
 print('Shape of data tensor:', data.shape)
 print('Shape of label tensor:', labels.shape)
 
@@ -56,11 +59,81 @@ y_val = labels[training_samples: training_samples + validation_samples]
 glove_dir = 'D:\\glove.6B'
 
 embeddings_index = {}
-f = open(os.path.join(glove_dir, 'globe.6B.100d.txt'))
+
+f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'), encoding='utf-8')
+
 for line in f:
     values = line.split()
     word = values[0]
     coefs = np.asarray(values[1:], dtype='float32')
     embeddings_index[word] = coefs
+
 f.close()
+
 print('Found %s word vectors.' % len(embeddings_index))
+
+# 准备GloVe词嵌入矩阵
+
+embedding_dim = 100
+
+embedding_matrix = np.zeros((max_words, embedding_dim))
+
+for word, i in word_index.items():
+    if i < max_words:
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+
+# 模型定义
+
+model = Sequential()
+model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
+
+# 在模型中加载GloVe嵌入
+
+model.layers[0].set_weights([embedding_matrix])
+model.layers[0].trainable = False
+
+# 训练与评估模型
+
+model.compile(
+    optimizer='rmsprop',
+    loss='binary_crossentropy',
+    metrics=['accurcy']
+)
+history = model.fit(
+    x_train,
+    y_train,
+    epochs=10,
+    batch_size=32,
+    validation_data=(x_val, y_val)
+)
+
+model.save_weights('pre_trained_glove_model.h5')
+
+# 绘制结果
+
+accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(accuracy) + 1)
+
+plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+plt.plot(epochs, val_accuracy, 'b', label='Validation accurcy')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
