c693899ae53f8ddd8f0c67fec81e101be1ac7ddc
liteng <930372551@qq.com>
2020-01-30 11:00:54 +0800

    6_16.imdb_lstm_bidirectional

---
+++ python_deep_learning/6_15.imdb_lstm_reverse.py
@@ -0,0 +1,35 @@
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+max_features = 10000
+maxlen = 500
+
+(x_train, y_train), (x_test, y_test) = imdb.load_data(
+    num_words=max_features
+)
+
+x_train = [x[::-1] for x in x_train]
+x_test = [x[::-1] for x in x_test]
+
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+
+model = Sequential()
+model.add(layers.Embedding(max_features, 128))
+model.add(layers.LSTM(32))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(
+    optimizer='rmsprop',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    x_train,
+    y_train,
+    batch_size=128,
+    validation_split=0.2
+)
