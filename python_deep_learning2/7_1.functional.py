d342abcdd8d68aa33cbf10b8b68d31faa82217a6
liteng <930372551@qq.com>
2020-01-31 09:59:04 +0800

    7_1.functional

---
+++ python_deep_learning2/7_1.functional.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras import layers
+from tensorflow.keras import Input
+
+seq_model = Sequential()
+seq_model.add(layers.Dense(32, activation='relu', input_shape=(64,)))
+seq_model.add(layers.Dense(32, activation='relu'))
+seq_model.add(layers.Dense(10, activation='softmax'))
+
+input_tensor = Input(shape=(64,))
+x = layers.Dense(32, activation='relu')(input_tensor)
+x = layers.Dense(32, activation='relu')(x)
+output_tensor = layers.Dense(10, activation='softmax')(x)
+
+model = Model(input_tensor, output_tensor)
+
+model.summary()
